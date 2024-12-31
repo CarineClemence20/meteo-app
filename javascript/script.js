08302d87f32fb5262ff8375c54bbaf0c230e53c0
GIKUNDIRO Clemence Carine <carineclememce20@gmail.com>
2024-12-31 16:12:49 +0200

    added a basic search engine to display city name

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+function displayWeatherInfo(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city-name");
+  cityElement.innerHTML = searchInputElement.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+
+searchFormElement.addEventListener("submit", displayWeatherInfo);
